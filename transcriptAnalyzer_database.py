d2eb75a1277cac021245dcc3e27118afa530c3f7
Lee Burnette <lee.burnette@wbgames.com>
2017-04-22 21:35:16 -0400

    Attempt to chunk message stuff so WF doesn't OOM-kill the process.

--- transcriptAnalyzer_database.py
+++ transcriptAnalyzer_database.py
@@ -162,10 +162,6 @@ def parse_convos(room_num=240, year=2016, month=3, day=23, hour_start=0, hour_en
     if len(transcript.messages) == 0:
         return
 
-    histories = {}
-    contents = {}
-    markdowns = {}
-
     hist = lambda n: lambda: histories.setdefault(n, ur.urlopen('https://chat.stackexchange.com/messages/{}/history'.format(n)).read().decode('utf-8'))
     cont = lambda n: lambda: contents.setdefault(n, ur.urlopen('https://chat.stackexchange.com/message/{}'.format(n)).read().decode('utf-8'))
     mark = lambda n: lambda: markdowns.setdefault(n, ur.urlopen('https://chat.stackexchange.com/messages/{}/{}'.format(room_num, n)).read().decode('utf-8'))
@@ -178,125 +174,144 @@ def parse_convos(room_num=240, year=2016, month=3, day=23, hour_start=0, hour_en
 
     if debug & 4:
         print("Starting the threads... ({} of them)".format(len(threads)))
-    counter = 0
-    chunk_size = 30
-    threads_to_run = []
-    while counter < len(threads):
+
+    db_counter = 0
+    db_chunk_size = 1500
+
+    while db_counter < len(threads):
         if debug & 8:
-            print("Running threads {}-{}...".format(counter, counter + chunk_size), end='')
+            print("Running DB chunk {}-{}".format(db_counter, db_counter + db_chunk_size))
 
-        threads_to_run = threads[counter:counter + chunk_size]
-        counter += chunk_size
+        histories = {}
+        contents = {}
+        markdowns = {}
+        transcript_msgs = {}
 
-        for t in threads_to_run:
-            t.start()
+        thread_chunk = threads[db_counter:db_counter + db_chunk_size]
+        db_counter += db_chunk_size
 
-        for t in threads_to_run:
-            t.join()
+        thread_counter = 0
+        thread_chunk_size = 30
+        threads_to_run = []
+        while thread_counter < len(thread_chunk):
+            if debug & 16:
+                print("Running threads {}-{}...".format(thread_counter, thread_counter + thread_chunk_size), end='')
+
+            threads_to_run = thread_chunk[thread_counter:thread_counter + thread_chunk_size]
+            thread_counter += thread_chunk_size
+
+            for t in threads_to_run:
+                t.start()
+
+            for t in threads_to_run:
+                t.join()
+
+            if debug & 16:
+                print("  Done!")
 
         if debug & 8:
-            print("  Done!")
+            print("Threads are done!")
+
+        ##############
+
+        # users
+        transcript_uids = set(m['uid'] for m in transcript.messages.values())
+        users_in_db = User.objects.filter(uid__in=transcript_uids)
+        uids_in_db = set(u.uid for u in users_in_db)
+
+        for new_uid in transcript_uids - uids_in_db:
+            User(uid=new_uid, latest_msg=0, latest_name='').save()
+
+        users_in_db = User.objects.filter(uid__in=transcript_uids)
+
+        if debug & 8:
+            print("User objects are done!")
+
+        # usernames
+        for mid, msg in transcript.messages.items():
+            user = users_in_db.get(uid=msg['uid'])
+
+            if not Username.objects.filter(user=user.id, name=msg['name']).exists():
+                Username(user=user, name=msg['name']).save()
+                user.latest_name = msg['name']
+                user.latest_msg = mid
+                user.save()
+            elif mid > user.latest_msg:
+                user.latest_name = msg['name']
+                user.latest_msg = mid
+                user.save()
+
+        if debug & 8:
+            print("Username objects are done!")
+
+        # messages
+        transcript_mids = sorted(list(transcript.messages.keys()))
+
+        # build from transcript + history + content + markdown
+        for mid in transcript_mids:
+            # transcript
+            msg = transcript.messages[mid].copy()
+
+            # miscellaneous
+            msg['mid'] = mid
+            msg['user'] = users_in_db.get(uid=msg['uid'])
+            msg['date'] = date
+            msg['room'] = room_num
+
+            # history
+            history = histories[mid]
+            msg['was_edited'] = "<b>edited:</b>" in history or "<b>said:</b>" not in history
+            # print(history)
+            hour, minute, half = re.search('<div class="timestamp".*(\d{1,2}):(\d\d) (A|P)M', history).groups()
+            hour = int(hour) % 12 + 12 * (half == "P")
+            minute = int(minute)
+            msg['time'] = datetime.time(hour, minute)
+
+            # content
+            msg['content'] = contents[mid]
+            msg_onebox = re.search('<div class="onebox ob-(\w+)', contents[mid])
+            msg['onebox'] = bool(msg_onebox)
+            if msg_onebox:
+                msg['onebox_type'] = msg_onebox.groups()[0]
+
+            # markdown
+            msg['markdown'] = markdowns[mid]
+
+            # finalize
+            msg.pop('uid')
+            transcript_msgs[mid] = msg
+
+        msgs_in_db = Message.objects.filter(mid__gte=transcript_mids[0], mid__lte=transcript_mids[-1])
+        mids_in_db = [m.mid for m in msgs_in_db]
+
+        message_num = 0
+        messages_to_create = []
+        for mid, message in transcript.messages.items():
+            if mid in mids_in_db:
+                mids_in_db.remove(mid)
+
+            if debug & 2:
+                print("message_num, mid: {}, {}" .format(message_num, mid))
+            message_num += 1
+
+            if mid not in mids_in_db:
+                messages_to_create.append(Message(**transcript_msgs[mid]))
+            else:
+                msgs_in_db.filter(mid=mid).update(**transcript_msgs[mid])
+
+        if debug & 8:
+            print("Message stuff done!")
+
+        # these were not in the transcript so should be deleted from the database
+        Message.objects.filter(mid__in=mids_in_db).delete()
+
+        Message.objects.bulk_create(messages_to_create)
+
+        if debug & 8:
+            print("Message table updated!")
 
     if debug & 4:
-        print("Threads are done!")
-    ##############
-
-    # users
-    transcript_uids = set(m['uid'] for m in transcript.messages.values())
-    users_in_db = User.objects.filter(uid__in=transcript_uids)
-    uids_in_db = set(u.uid for u in users_in_db)
-
-    for new_uid in transcript_uids - uids_in_db:
-        User(uid=new_uid, latest_msg=0, latest_name='').save()
-
-    users_in_db = User.objects.filter(uid__in=transcript_uids)
-
-    if debug & 4:
-        print("User objects are done!")
-
-    # usernames
-    for mid, msg in transcript.messages.items():
-        user = users_in_db.get(uid=msg['uid'])
-
-        if not Username.objects.filter(user=user.id, name=msg['name']).exists():
-            Username(user=user, name=msg['name']).save()
-            user.latest_name = msg['name']
-            user.latest_msg = mid
-            user.save()
-        elif mid > user.latest_msg:
-            user.latest_name = msg['name']
-            user.latest_msg = mid
-            user.save()
-
-    if debug & 4:
-        print("Username objects are done!")
-
-    # messages
-    transcript_mids = sorted(list(transcript.messages.keys()))
-
-    # build from transcript + history + content + markdown
-    transcript_msgs = {}
-    for mid in transcript_mids:
-        # transcript
-        msg = transcript.messages[mid].copy()
-
-        # miscellaneous
-        msg['mid'] = mid
-        msg['user'] = users_in_db.get(uid=msg['uid'])
-        msg['date'] = date
-        msg['room'] = room_num
-
-        # history
-        history = histories[mid]
-        msg['was_edited'] = "<b>edited:</b>" in history or "<b>said:</b>" not in history
-        # print(history)
-        hour, minute, half = re.search('<div class="timestamp".*(\d{1,2}):(\d\d) (A|P)M', history).groups()
-        hour = int(hour) % 12 + 12 * (half == "P")
-        minute = int(minute)
-        msg['time'] = datetime.time(hour, minute)
-
-        # content
-        msg['content'] = contents[mid]
-        msg_onebox = re.search('<div class="onebox ob-(\w+)', contents[mid])
-        msg['onebox'] = bool(msg_onebox)
-        if msg_onebox:
-            msg['onebox_type'] = msg_onebox.groups()[0]
-
-        # markdown
-        msg['markdown'] = markdowns[mid]
-
-        # finalize
-        msg.pop('uid')
-        transcript_msgs[mid] = msg
-
-    msgs_in_db = Message.objects.filter(mid__gte=transcript_mids[0], mid__lte=transcript_mids[-1])
-    mids_in_db = [m.mid for m in msgs_in_db]
-
-    message_num = 0
-    messages_to_create = []
-    for mid, message in transcript.messages.items():
-        if mid in mids_in_db:
-            mids_in_db.remove(mid)
-
-        if debug & 2:
-            print("message_num, mid: {}, {}" .format(message_num, mid))
-        message_num += 1
-
-        if mid not in mids_in_db:
-            messages_to_create.append(Message(**transcript_msgs[mid]))
-        else:
-            msgs_in_db.filter(mid=mid).update(**transcript_msgs[mid])
-
-    if debug & 4:
-        print("Message stuff done!")
-
-    # these were not in the transcript so should be deleted from the database
-    Message.objects.filter(mid__in=mids_in_db).delete()
-
-    Message.objects.bulk_create(messages_to_create)
-
-    if debug & 4:
-        print("Message table updated!")
+        print("All messages done!")
 
     if create_snapshot or snapshot:
         # create or update snapshot
