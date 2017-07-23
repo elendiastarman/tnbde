eb34ea7b2996d0a2ebd710e170a1a37d9dd300d5
Lee Burnette <lee.burnette@wbgames.com>
2017-07-22 23:21:31 -0400

    Stuff the code into a function.

--- migrations/query_migration.py
+++ migrations/query_migration.py
@@ -63,40 +63,42 @@ priority_shortcodes = [
   'mtkmqSzFCA',
 ]
 
-seen_codes = [inquiry.shortcode for inquiry in Inquiry.objects.all()]
 
-print(os.getcwd())
+def migrate_query():
+  seen_codes = [inquiry.shortcode for inquiry in Inquiry.objects.all()]
 
-for filename in os.listdir(os.path.join(os.getcwd(), 'queries')):
-  print(filename)
-  break
+  print(os.getcwd())
 
-  shortcode = filename[:10]
-  if shortcode not in seen_codes:
-    with open("{}JS.txt".format(shortcode)) as js_file:
-      js = js_file.read()
-      js_sha1 = hashlib.sha1(bytes(js, encoding='utf-8')).hexdigest()
+  for filename in os.listdir(os.path.join(os.getcwd(), 'queries')):
+    print(filename)
+    break
 
-      try:
-        inquiry = Inquiry.objects.get(sha1=js_sha1)
+    shortcode = filename[:10]
+    if shortcode not in seen_codes:
+      with open("{}JS.txt".format(shortcode)) as js_file:
+        js = js_file.read()
+        js_sha1 = hashlib.sha1(bytes(js, encoding='utf-8')).hexdigest()
 
-        if inquiry.shortcode != shortcode and shortcode in priority_shortcodes:
-          inquiry.shortcode = shortcode
+        try:
+          inquiry = Inquiry.objects.get(sha1=js_sha1)
+
+          if inquiry.shortcode != shortcode and shortcode in priority_shortcodes:
+            inquiry.shortcode = shortcode
+            inquiry.save()
+            continue
+
+        except ObjectDoesNotExist:
+          inquiry = Inquiry(js=js, sha1=js_sha1)
+
+          with open("{}In.txt".format(shortcode)) as sql_file:
+            sql = sql_file.read()
+            sql_sha1 = hashlib.sha1(bytes(sql, encoding='utf-8')).hexdigest()
+
+            try:
+              query = Query.objects.get(sha1=sql_sha1)
+            except ObjectDoesNotExist:
+              query = Query(sql=sql, sha1=sql_sha1)
+              query.save()
+
+          inquiry.query = query
           inquiry.save()
-          continue
-
-      except ObjectDoesNotExist:
-        inquiry = Inquiry(js=js, sha1=js_sha1)
-
-        with open("{}In.txt".format(shortcode)) as sql_file:
-          sql = sql_file.read()
-          sql_sha1 = hashlib.sha1(bytes(sql, encoding='utf-8')).hexdigest()
-
-          try:
-            query = Query.objects.get(sha1=sql_sha1)
-          except ObjectDoesNotExist:
-            query = Query(sql=sql, sha1=sql_sha1)
-            query.save()
-
-        inquiry.query = query
-        inquiry.save()
